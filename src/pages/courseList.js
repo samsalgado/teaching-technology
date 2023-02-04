9b04240670e429d711b94762055e828ee1cd93b3
THEMERLINGROUP <67569156+THEMERLINGROUP@users.noreply.github.com>
2023-02-04 13:27:28 -0500

    courList is uploaded

---
+++ src/pages/courseList.js
@@ -0,0 +1,9 @@
+const CourseList = {
+bnb: require("../pics/bnb.webp"),
+cplusplus: require("../pics/c++.webp"),
+golang: require("../pics/golang.webp"),
+node: require("../pics/react.webp"),
+bitcoin: require("../pics/btc.webp"),
+python: require("../pics/python.webp")
+};
+export default CourseList;
